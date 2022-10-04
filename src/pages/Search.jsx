6f03bfe1bd828bfc719d7d12ba345e8cb2c7605b
jaidernunes <jaidernf@gmail.com>
2022-10-04 17:38:49 -0300

    Req 5: Pass

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,10 +2,38 @@ import React from 'react';
 import Header from '../components/Header';
 
 export default class Search extends React.Component {
+  state = { artistInput: '' };
+
+  handleSearch = ({ target }) => {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    });
+  };
+
   render() {
+    const { state } = this;
     return (
       <div data-testid="page-search">
         <Header />
+        <form>
+          <label htmlFor="artistInput">
+            <input
+              type="text"
+              id="artistInput"
+              name="artistInput"
+              data-testid="search-artist-input"
+              onChange={ this.handleSearch }
+            />
+          </label>
+          <button
+            type="button"
+            data-testid="search-artist-button"
+            disabled={ state.artistInput.length < 2 }
+          >
+            Pesquisar
+          </button>
+        </form>
       </div>
     );
   }
