9b73711964b336845a72e1bcbe95cb7e8b488ea0
jaidernunes <jaidernf@gmail.com>
2022-10-05 12:20:13 -0300

    Req 6: Pass

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,8 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
+import Loading from '../components/Loading';
+import searchAlbumsAPI from '../services/searchAlbumsAPI';
 
 export default class Search extends React.Component {
-  state = { artistInput: '' };
+  state = {
+    artistInput: '',
+    loading: false,
+    lastSearch: '',
+    foundAlbums: {},
+  };
 
   handleSearch = ({ target }) => {
     const { name, value } = target;
@@ -11,8 +19,27 @@ export default class Search extends React.Component {
     });
   };
 
+  searchClick = async () => {
+    const { artistInput } = this.state;
+
+    this.setState({
+      loading: true,
+      lastSearch: artistInput,
+      artistInput: '',
+    });
+
+    const albumsAPI = await searchAlbumsAPI(artistInput);
+    console.log(albumsAPI);
+
+    this.setState({
+      foundAlbums: albumsAPI,
+      loading: false,
+    });
+  };
+
   render() {
     const { state } = this;
+    const { artistInput, lastSearch, foundAlbums, loading } = state;
     return (
       <div data-testid="page-search">
         <Header />
@@ -24,15 +51,52 @@ export default class Search extends React.Component {
               name="artistInput"
               data-testid="search-artist-input"
               onChange={ this.handleSearch }
+              value={ artistInput }
             />
           </label>
-          <button
-            type="button"
-            data-testid="search-artist-button"
-            disabled={ state.artistInput.length < 2 }
-          >
-            Pesquisar
-          </button>
+
+          { loading ? <Loading />
+            : (
+              <button
+                type="button"
+                data-testid="search-artist-button"
+                disabled={ artistInput.length < 2 }
+                onClick={ this.searchClick }
+              >
+                Pesquisar
+              </button>)}
+
+          {(lastSearch.length >= 2 && foundAlbums.length >= 1) && (
+            <h5>
+              Resultado de álbuns de:
+              {' '}
+              {lastSearch}
+            </h5>
+          )}
+
+          { foundAlbums.length >= 1 ? (foundAlbums.map((album) => (
+            <Link
+              to={ `/album/${album.collectionId}` }
+              data-testid={ `link-to-album-${album.collectionId}` }
+              key={ album.collectionId }
+            >
+              <div>
+                <img
+                  src={ album.artworkUrl100 }
+                  alt={ `Cover of the album ${album.collectionName}` }
+                />
+                <h3>
+                  { album.collectionName }
+                </h3>
+                <h5>
+                  {album.artistName}
+                </h5>
+              </div>
+            </Link>
+          ))) : (
+            <h3>Nenhum álbum foi encontrado</h3>
+          )}
+
         </form>
       </div>
     );
